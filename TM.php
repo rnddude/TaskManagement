d9a265a794c757e7ea8f28b68c790d930bccc198
rnddude <rnddude@icloud.com>
2014-02-25 10:53:39 +0100

    removed GetUserName

--- TM.php
+++ TM.php
@@ -46,35 +46,4 @@ $wgHooks[ 'sfWritePageData' ][] = 'TMUtils::notifyAssignees';
 // Languages & Aliases
 $wgExtensionMessagesFiles[ 'TM' ] = $tmIP . '/TM.i18n.php';
 $wgExtensionMessagesFiles[ 'TMAlias' ] = $tmIP . '/TM.alias.php';
-
-/*
-* The following code is part of the GetUserName extension (https://www.mediawiki.org/wiki/Extension%3aGetUserName) by Ejcaputot
-* and is distributed under the GPL license. For convenience it has been integrated.
-*/
-$wgHooks[ 'ParserFirstCallInit' ][] = 'ExtGetUserName::setup';
-$wgHooks[ 'LanguageGetMagic' ][]  = 'ExtGetUserName::languageGetMagic';
-class ExtGetUserName {
-    private static $parserFunctions = array(
-        'USERNAME' => 'getUserName',
-    );
- 
-    public static function setup( &$parser ) {
-        // register each hook
-        foreach( self::$parserFunctions as $hook => $function )
-            $parser->setFunctionHook( $hook,
-                array( __CLASS__, $function ), SFH_OBJECT_ARGS );
- 
-        return true;
-    }
- 
-    public static function languageGetMagic( &$magicWords, $langCode ) {
-        $magicWords[ 'USERNAME' ] = array( 0, 'USERNAME' ); 
-        return true;
-    }
- 
-    public static function getUserName( &$parser, $frame, $args ) {
-        $parser->disableCache();
-	global $wgUser;
-        return trim( $wgUser->getName() );
-    }
 }
